7bfc1f6121f31289c048191dbe6029889a7990d4
mattmalicki <mateusz.malicki96@gmail.com>
2023-09-10 18:05:31 +0200

    e

---
+++ src/components/Profile.jsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import css from '../css/Profile.module.css';
+
+export default function Profile({
+  username,
+  tag,
+  location,
+  imgUrl,
+  followers,
+  views,
+  likes,
+}) {
+  reutrn(
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={imgUrl} alt={username} className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.PropTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  imgUrl: PropTypes.string,
+  followers: PropTypes.number,
+  views: PropTypes.number,
+  likes: PropTypes.number,
+};

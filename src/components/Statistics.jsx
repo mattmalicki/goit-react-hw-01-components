2d1d606b956b9c36b93c1a883e57823b4425eb2a
mattmalicki <mateusz.malicki96@gmail.com>
2023-09-15 14:47:51 +0200

    fixing styles for stats

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -14,7 +14,7 @@ export function Statistics({ title, stats }) {
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
-      <ul className={css.statList}>{listItems}</ul>
+      <ul className={css['stat-list']}>{listItems}</ul>
     </section>
   );
 }
